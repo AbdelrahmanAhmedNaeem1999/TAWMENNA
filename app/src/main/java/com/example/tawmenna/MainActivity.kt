98d33b9bbd203379a67a38c28948b964abac4100
Abdelrahman Ahmed <AbdelrahmanAhmedNaeem1999>
2021-01-30 20:18:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tawmenna/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tawmenna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolBar))
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val appBarConfiguration= AppBarConfiguration(setOf(R.layout.fragment_home,R.layout.fragment_cart,R.layout.fragment_profile))
+        val navController =findNavController(R.id.fragment)
+        setupActionBarWithNavController(navController ,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
